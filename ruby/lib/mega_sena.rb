006d3ba4407df3ce04c73ba0e84999cd09281f59
Elias Ferreira Junior <eliaswebdev@gmail.com>
2019-03-19 18:03:46 -0300

    add sorder method

--- ruby/lib/mega_sena.rb
+++ ruby/lib/mega_sena.rb
@@ -9,4 +9,16 @@ class MegaSena
   def shuffle_numbers
     numbers.shuffle
   end
+
+  def sorter
+    original = shuffle_numbers
+    sorted = []
+
+    6.times do
+      i = original.shuffle[0]
+      sorted << i
+      original.delete(i)
+    end
+    sorted.sort.join(' - ')
+  end
 end
